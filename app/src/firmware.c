c97180ada1e6ece9d0ac25fb2debe25f02bb3e0c
Leandro Soria <leandrosoria@hotmail.com>
2024-01-02 19:59:57 -0300

    Initial commit. Setup libopencm3 submodule, vscode helper configs and make a blinky

---
+++ app/src/firmware.c
@@ -0,0 +1,34 @@
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+
+#define LED_PORT (GPIOB)
+#define LED_PIN  (GPIO0)
+
+static void rcc_setup(void)
+{
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_96MHZ]);
+}
+
+static void gpio_setup(void)
+{
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+}
+
+static void delay_cycles(uint32_t cycles)
+{
+    for(uint32_t i = 0; i < cycles; i++) { __asm__("nop"); }
+}
+
+int main(void)
+{
+    rcc_setup();
+    gpio_setup();
+
+    while(1) {
+        gpio_toggle(LED_PORT, LED_PIN);
+        delay_cycles(96000000 / 4);
+    }
+
+    return 0;
+}
