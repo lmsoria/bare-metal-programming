070888f5bc006ee712cc5720a96a701b36443f35
Leandro Soria <leandrosoria@hotmail.com>
2024-02-05 22:03:37 -0300

    Refactor bootloader load process

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,3 +1,4 @@
+#include <libopencm3/cm3/vector.h>
 #include <libopencm3/stm32/memorymap.h>
 
 #include "common-defines.h"
@@ -5,23 +6,17 @@
 #define BOOTLOADER_SIZE (0x8000U) // 32KB
 #define MAIN_APP_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE)
 
-static void jump_to_main(void);
+static void jump_to_main_firmware(void);
 
-static void jump_to_main(void)
+static void jump_to_main_firmware(void)
 {
-    typedef void (*void_function)(void);
-
-    uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U); // First entry of MAIN_APP_START_ADDRESS is the Stack Pointer, so jumpt to the next index to get the Reset Vector
-    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-
-    void_function jump_function = (void_function)(reset_vector);
-
-    jump_function();
+    vector_table_t* main_vector_table = (vector_table_t*)(MAIN_APP_START_ADDRESS);
+    main_vector_table->reset();
 }
 
 int main(void)
 {
-    jump_to_main();
+    jump_to_main_firmware();
 
     // Never return
     return 0;
